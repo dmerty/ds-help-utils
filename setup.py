90083e4fc75a64a222e92ae6d873c56c2894fd46
Дмитрий Емельянов <dimen@MacBook-Pro-Dmitrij-704.local>
2023-10-02 14:00:11 +0300

    changes for pandas 2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email="asdqwer92@gmail.com",
     packages=find_packages(),
     install_requires=[
-        "numpy>=1.23.1",
+        "numpy==1.23.5",
         "pandas>=2.0.0",
         "scikit-learn>=1.1.1"
     ]
